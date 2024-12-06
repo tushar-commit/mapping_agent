d2fa181ca09f8fd45074c7fba620f8acdbd64bd0
Puneet <puneet@stepfunction.ai>
2024-12-06 11:38:23 +0530

    minor changes

--- app.py
+++ app.py
@@ -235,6 +235,7 @@ def run_app():
                     if view.display_button("Confirm Finish"):
                         view.show_message("Thank you for using the Column Mapping App!", "success")
                         session.clear()
-
+                        view.rerun_script()
+                        
 if __name__ == "__main__":
     run_app()
